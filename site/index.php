bcdd0ed3915932a2957add4fea96e39e10650bba
Murilo Gimenez <91744500+murilouwu@users.noreply.github.com>
2022-10-23 01:32:43 -0300

    Add files via upload

---
+++ site/index.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="css/cssMain.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="shortcut icon" href="imgs/logo.png">
+	<title>Home</title>
+</head>
+<body>
+ 	<div class="topo">
+ 		<div class="topo-nav">
+ 			<div class="topo-nvlogo"></div>
+ 			<div class="topo-nvlinks">
+ 				<button class="Nvlink">PEDIDOS</button>
+ 				<button class="Nvlink">PERFIL</button>
+ 				<button class="Nvlink">PRODUTOS</button>
+ 				<button class="Nvlink">COSMÉTICOS</button>
+ 			</div>
+ 			<i class="fa-solid fa-magnifying-glass NvLupa"></i>
+ 			<button class="topo-nvLogin">SIGN IN OR LOGIN</button>
+ 		</div>
+ 		<div class="topo-body">
+ 			<div class="BdTextDiv">
+ 				<h1 class="BdTitle">Somos</h1>
+				<p class="BdText">
+
+					SIGMAMEP e Vedemos produtos de estética e qualidade, para apreciodores de jogos, que desejam um produto sigma (sigma > alfa), tanto para aprimorar seu Computador, Celular, Tablet Gamer, tanto aprimorar sua estética sigmaGamer.
+				</p>	
+ 			</div>
+ 			<div class="Bdbaner">
+ 				<div class="Bdbanertapa">
+ 					<h1 class="tapaTitle"><span>SIGMAMEP</span>SIGMAMEP</h1>
+ 				</div>
+				<img src="imgs/moduraAdd.png" class="Ban-MolimgA">
+				<img src="imgs/Baner.png" class="Ban-MolimgB">
+				<div class="Bdbanertapa2"></div>
+ 			</div>
+ 		</div>
+ 	</div>
+ 	<div class="centro">
+ 		<h1 class="CenterText">Categoria de Produtos</h1>
+ 		<div class="Crt-Scroll">
+ 			<div class="Crt-Scrollitens">
+	 			<div class="itCard">
+	 				<i class="icon fa-sharp fa-solid fa-desktop"></i>
+	 				<label class="itCd-name">name</label>
+	 			</div>
+	 			<div class="itCard">
+	 				<i class="icon fa-sharp fa-solid fa-desktop"></i>
+	 				<label class="itCd-name">name</label>
+	 			</div>
+	 			<div class="itCard">
+	 				<i class="icon fa-sharp fa-solid fa-desktop"></i>
+	 				<label class="itCd-name">name</label>
+	 			</div>
+	 			<div class="itCard">
+	 				<i class="icon fa-sharp fa-solid fa-desktop"></i>
+	 				<label class="itCd-name">name</label>
+	 			</div>
+	 			<div class="itCard">
+	 				<i class="icon fa-sharp fa-solid fa-desktop"></i>
+	 				<label class="itCd-name">name</label>
+	 			</div>
+	 			<div class="itCard">
+	 				<i class="icon fa-sharp fa-solid fa-desktop"></i>
+	 				<label class="itCd-name">name</label>
+	 			</div>
+	 			<div class="itCard">
+	 				<i class="icon fa-sharp fa-solid fa-desktop"></i>
+	 				<label class="itCd-name">name</label>
+	 			</div>
+	 			<div class="itCard">
+	 				<i class="icon fa-sharp fa-solid fa-desktop"></i>
+	 				<label class="itCd-name">name</label>
+	 			</div>
+	 			<div class="itCard">
+	 				<i class="icon fa-sharp fa-solid fa-desktop"></i>
+	 				<label class="itCd-name">name</label>
+	 			</div>
+	 		</div>
+ 		</div>
+ 		<div class="Crt-Result">
+ 			<h1 class="Ctr-Name">Name category escolhida</h1>
+ 			<div class="Crt-PageCategory">
+				<div class="PgCard">
+					<div class="PgCd-Img"></div>
+					<div class="PgCd-Data">
+						<label class="PgCdDt-Name">name</label>
+						<label class="PgCdDt-Price">000000,00</label>
+					</div>
+				</div>
+				<div class="PgCard">
+					<div class="PgCd-Img"></div>
+					<div class="PgCd-Data">
+						<label class="PgCdDt-Name">name</label>
+						<label class="PgCdDt-Price">000000,00</label>
+					</div>
+				</div>
+				<div class="PgCard">
+					<div class="PgCd-Img"></div>
+					<div class="PgCd-Data">
+						<label class="PgCdDt-Name">name</label>
+						<label class="PgCdDt-Price">000000,00</label>
+					</div>
+				</div>
+				<div class="PgCard">
+					<div class="PgCd-Img"></div>
+					<div class="PgCd-Data">
+						<label class="PgCdDt-Name">name</label>
+						<label class="PgCdDt-Price">000000,00</label>
+					</div>
+				</div>
+				<div class="PgCard">
+					<div class="PgCd-Img"></div>
+					<div class="PgCd-Data">
+						<label class="PgCdDt-Name">name</label>
+						<label class="PgCdDt-Price">000000,00</label>
+					</div>
+				</div>
+				<div class="PgCard">
+					<div class="PgCd-Img"></div>
+					<div class="PgCd-Data">
+						<label class="PgCdDt-Name">name</label>
+						<label class="PgCdDt-Price">000000,00</label>
+					</div>
+				</div>
+				<div class="PgCard">
+					<div class="PgCd-Img"></div>
+					<div class="PgCd-Data">
+						<label class="PgCdDt-Name">name</label>
+						<label class="PgCdDt-Price">000000,00</label>
+					</div>
+				</div>
+				<div class="PgCard">
+					<div class="PgCd-Img"></div>
+					<div class="PgCd-Data">
+						<label class="PgCdDt-Name">name</label>
+						<label class="PgCdDt-Price">000000,00</label>
+					</div>
+				</div>
+				<div class="PgCard">
+					<div class="PgCd-Img"></div>
+					<div class="PgCd-Data">
+						<label class="PgCdDt-Name">name</label>
+						<label class="PgCdDt-Price">000000,00</label>
+					</div>
+				</div>
+				<div class="PgCard">
+					<div class="PgCd-Img"></div>
+					<div class="PgCd-Data">
+						<label class="PgCdDt-Name">name</label>
+						<label class="PgCdDt-Price">000000,00</label>
+					</div>
+				</div>
+				<div class="PgCard">
+					<div class="PgCd-Img"></div>
+					<div class="PgCd-Data">
+						<label class="PgCdDt-Name">name</label>
+						<label class="PgCdDt-Price">000000,00</label>
+					</div>
+				</div>
+ 			</div>
+ 			<div class="Crt-control">
+				<button class="btn-control">Mais Produtos</button>
+			</div>
+ 		</div>
+ 	</div>
+ 	<div class="footer">
+
+ 		Footer
+ 	</div>
+ 	<script src="https://kit.fontawesome.com/39cab4bf95.js" crossorigin="anonymous"></script>
+</body>
+</html>
